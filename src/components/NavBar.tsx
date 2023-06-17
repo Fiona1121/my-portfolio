907cf1228f9180f4d9ea66be6d853f41e92febec
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2023-06-17 12:40:05 +0800

    create header + home page hero section

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import { GithubIcon, LinkedInIcon } from "./Icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+const CustomLink = ({
+  href,
+  title,
+  className = "",
+}: {
+  href: string;
+  title: string;
+  className?: string;
+}) => {
+  const pathname = usePathname();
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[2px] inline-block bg-dark absolute left-0 -bottom-1 group-hover:w-full transition-[width] duration-300 ease-in-out ${
+          pathname === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
+
+const NavBar = (props: Props) => {
+  return (
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="mx-4" />
+      </nav>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://www.linkedin.com/"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com/Fiona1121"
+          target={"_blank"}
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <GithubIcon />
+        </motion.a>
+      </nav>
+      <div className="absolute left-1/2 top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
